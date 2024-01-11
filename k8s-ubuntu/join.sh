33f23b9179b5f4a29a4ae9dabecdf4002ac95142
zipkero <zipkero@gmail.com>
2024-01-11 22:35:24 +0900

    change version

--- k8s-ubuntu/join.sh
+++ k8s-ubuntu/join.sh
@@ -1,1 +1,1 @@
-kubeadm join 192.168.56.10:6443 --token 578f2o.n9yf6q7z8y4i89xn --discovery-token-ca-cert-hash sha256:df147c82ee7f9f17375983055702534113df208aa82cb3509e9d5421f4258475 
+kubeadm join 192.168.56.10:6443 --token miy36p.r8vva34sgkwokzt7 --discovery-token-ca-cert-hash sha256:c24e8e04ea51f831bccb5b36188509d48b345f7d763ff0ff565505c12834cb62 
