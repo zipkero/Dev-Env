7d4a9d62aaa3595c91fc107c2fc6f5b7d1e0199b
zipkero <zipkero@gmail.com>
2024-01-13 02:05:45 +0900

    오류 수정

--- k8s-ubuntu/join.sh
+++ k8s-ubuntu/join.sh
@@ -1,1 +1,1 @@
-kubeadm join 192.168.56.10:6443 --token miy36p.r8vva34sgkwokzt7 --discovery-token-ca-cert-hash sha256:c24e8e04ea51f831bccb5b36188509d48b345f7d763ff0ff565505c12834cb62 
+kubeadm join 192.168.56.10:6443 --token 2w8prk.12qb74wt4he0wil3 --discovery-token-ca-cert-hash sha256:1663a0f71c8aff56c099c00f5e350cc444248f1101dd7ce6249236c030c966d4 
