ea24e14a17988d0e08d523bf0e240d094118df5a
kero <kero@richalien.net>
2024-01-16 14:31:30 +0900

    오류 수정

--- k8s-ubuntu/join.sh
+++ k8s-ubuntu/join.sh
@@ -1,1 +1,1 @@
-kubeadm join 192.168.56.10:6443 --token bfqwem.z2o2mlaprw6orf57 --discovery-token-ca-cert-hash sha256:5198c8487f911d024ad83fb3697530560d6008de466ee0c1b935baf113353aad 
+kubeadm join 192.168.56.10:6443 --token q1y9bl.ejynej0j7bkg4b9f --discovery-token-ca-cert-hash sha256:e07384681c6b91d2f602ba25904bb81b47e511796232ef00653fd92507a4df85 
